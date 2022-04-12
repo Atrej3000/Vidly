72e2541631cb7e29f7fc80f48bf53fbe9699c672
Andrii Serha <andrej.serga@gmail.com>
2022-04-12 19:17:41 +0300

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
+import React, { Component } from "react";
 import Movies from "./components/movies";
+import { getMovies } from "./services/fakeMovieService";
 
-function App() {
-  return (
-    <main className="container">
-      <Movies />
-    </main>
-  );
+class App extends Component {
+  state = {
+    movies: getMovies(),
+  };
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+  render() {
+    return (
+      <main className="container">
+        <Movies movies={this.state.movies} onDelete={this.handleDelete} />
+      </main>
+    );
+  }
 }
 
 export default App;
