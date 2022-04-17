8d47c5113031473d417dda1fa12ab3d087ecec7a
Andrii Serha <andrej.serga@gmail.com>
2022-04-17 22:39:45 +0300

    Refactor like component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
-import Like from "./like";
+import Like from "./common/like";
+import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
   render() {
-    const { length: count } = this.props.movies;
+    const { length: count } = this.state.movies;
     if (count === 0) {
       return <p>There are no movies in the database</p>;
     }
@@ -18,21 +33,25 @@ class Movies extends Component {
               <th>Stock</th>
               <th>Rate</th>
               <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            {this.props.movies.map((movie) => (
+            {this.state.movies.map((movie) => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
                 <td>
-                  <Like />
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
                 </td>
                 <td>
                   <button
-                    onClick={() => this.props.onDelete(movie)}
+                    onClick={() => this.handleDelete(movie)}
                     className="btn btn-danger btn-sm"
                   >
                     Delete
